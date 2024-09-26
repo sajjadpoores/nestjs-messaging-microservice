9cc850f154bce22e19c7f914d59df2c47469a03d
sajjadpoores <sajjadpoores@gmail.com>
2024-09-26 20:42:49 +0330

    feature(user/auth/controller): added api tag for swagger

--- user/src/modules/auth/auth.controller.ts
+++ user/src/modules/auth/auth.controller.ts
@@ -1,12 +1,15 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterUserBodyDto } from './dto/register-user-body.dto';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @Controller('auth')
+@ApiTags('Auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('register')
+  @ApiOperation({ summary: 'register a new user' })
   async register(@Body() body: RegisterUserBodyDto) {
     return this.authService.register(body);
   }
