ba1e0e68c6dd2a8be5cbb92116924a87f6c886db
sajjadpoores <sajjadpoores@gmail.com>
2024-09-26 21:46:21 +0330

    feature(user/user): added user update api

--- user/src/modules/user/user.controller.ts
+++ user/src/modules/user/user.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Put } from '@nestjs/common';
 import { UserService } from './user.service';
 import { GetUserParamDto } from './dto/get-user-param.dto';
 import { ApiTags } from '@nestjs/swagger';
+import { UpdateUserBodyDto } from './dto/update-user-body.dto';
 
 @Controller('user')
 @ApiTags('User')
@@ -11,5 +12,13 @@ export class UserController {
   @Get(':id')
   async getUser(@Param() params: GetUserParamDto) {
     return this.userService.getUser(params.id);
+  }
+
+  @Put(':id')
+  async updateUser(
+    @Param() params: GetUserParamDto,
+    @Body() body: UpdateUserBodyDto,
+  ) {
+    return this.userService.updateUser(params.id, body);
   }
 }
--- user/src/modules/user/user.service.ts
+++ user/src/modules/user/user.service.ts
@@ -1,5 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { UserRepository } from 'src/shared/repository/user.repository';
+import { UpdateUserBodyDto } from './dto/update-user-body.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
@@ -16,5 +18,22 @@ export class UserService {
       username: user.username,
       name: user.name,
     };
+  }
+
+  async updateUser(userId: number, body: UpdateUserBodyDto) {
+    const user = await this.userRepository.findOne({ where: { id: userId } });
+    if (!user) {
+      throw new NotFoundException("User doesn't exist!");
+    }
+
+    user.name = body.name || user.name;
+    user.username = body.username || user.name;
+    if (body.password) {
+      const password = await bcrypt.hash(body.password, 10);
+      user.password = password;
+    }
+
+    await user.save();
+    return { message: 'User updated successfully', user };
   }
 }
