1a7128e5f9f23fefeec35453c729c89802b7b117
sajjadpoores <sajjadpoores@gmail.com>
2024-09-26 19:10:07 +0330

    feature(env, main.ts, app.module): added config module to project to setup envs

--- user/src/app.module.ts
+++ user/src/app.module.ts
@@ -1,7 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: join(
+        __dirname,
+        '../env/',
+        `${process.env.NODE_ENV || 'development'}.env`,
+      ),
+      isGlobal: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
